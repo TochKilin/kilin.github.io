124f6eba16713eedea5c388e14740fae3744e8c7
TochKilin <kilin2002kilin@gmail.com>
2025-02-09 12:22:44 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+// function hello(){
+//     document.getElementById("btn-1").style.display="block";
+// }
+function callphone(){
+    window.location.href="tel:+885071885688";
+}
+function opentelegram(){
+    window.location.href="https://www.tiktok.com/@tochkilin?_t=ZS-8tkRUPKJyRG&_r=1";
+}
